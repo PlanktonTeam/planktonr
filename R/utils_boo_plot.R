ef2bc9bc0124f0a90dae4017d5f1e647e922a752
clairedavies <21113590+clairedavies@users.noreply.github.com>
2021-09-09 17:53:59 +1000

    function with quosure!!

--- R/utils_boo_plot.R
+++ R/utils_boo_plot.R
@@ -16,13 +16,12 @@ pr_get_PlotCols <- function(pal, n){
 }
 
 
-#' To product the base map of Australia for plotting
+#' To produce the base map of Australia for plotting
 #'
 #' @return an sf object for plotting the base map of Australia
 #' @export
 #'
 #' @import sf
-#' @import rgeos
 #'
 #' @examples
 #' MapOz <- pr_get_MapOz()
@@ -30,4 +29,31 @@ pr_get_MapOz <- function(){
   MapOz <- rnaturalearth::ne_countries(scale = "medium", country = "Australia",
                                        returnclass = "sf")
   return(MapOz)
+}
+
+#' To produce the climatology for plotting
+#'
+#' @param df data frame containing columns Year, Month, Day
+#' @param x Year, Month, Day, time period of climatology
+#'
+#' @return a dataframe for plotting
+#' @export
+#'
+#' @examples
+#' df <- data.frame(Month = rep(1:12,10), Code = 'NSI', Values = runif(120, min=0, max=10))
+#' pr_make_climatology(df, Month)
+#' @import dplyr
+#' @importFrom stats sd
+#' @importFrom magrittr "%>%"
+#' @importFrom rlang .data
+pr_make_climatology <- function(df, x){
+  x <- dplyr::enquo(arg = x)
+  df_climate <- df %>% dplyr::filter(!!x != 'NA') %>% # need to drop NA from month, added to dataset by complete(Year, Code)
+    dplyr::group_by(!!x, .data$Code) %>%
+    dplyr::summarise(mean = mean(.data$Values, na.rm = TRUE),
+                     N = length(.data$Values),
+                     sd = stats::sd(.data$Values, na.rm = TRUE),
+                     se = sd / sqrt(.data$N),
+                     .groups = "drop")
+  return(df_climate)
 }
