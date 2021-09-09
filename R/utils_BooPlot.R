f6d0d0869acf8b4452d765455d3e2229fd248325
clairedavies <21113590+clairedavies@users.noreply.github.com>
2021-09-09 12:54:15 +1000

    add initial booplot functions

---
+++ R/utils_BooPlot.R
@@ -0,0 +1,28 @@
+#' Setting the colours for plots using cmocean
+#'
+#' @param pal is the palette name
+#' @param n is the number of colours required
+#'
+#' @return is a list of colours of length n from palette pal
+#' @export
+#'
+#' @examples
+#' plotCols <- pr_get_PlotCols('matter', 5)
+pr_get_PlotCols <- function(pal, n){
+  plotCols <- cmocean::cmocean(pal)(n)
+  return(plotCols)
+}
+
+
+#' To product the base map of Australia for plotting
+#'
+#' @return an sf object for plotting the base map of Australia
+#' @export
+#'
+#' @examples
+#' MapOz <- pr_get_MapOz()
+pr_get_MapOz <- function(){
+  MapOz <- rnaturalearth::ne_countries(scale = "medium", country = "Australia",
+                                       returnclass = "sf")
+  return(MapOz)
+}
