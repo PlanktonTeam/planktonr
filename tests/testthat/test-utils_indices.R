39dceec42097e8cc4dc9dc35908ea491df1754c2
Jason Everett <jaseeverett@gmail.com>
2022-06-27 17:47:45 +1000

    Increase testing

--- tests/testthat/test-utils_indices.R
+++ tests/testthat/test-utils_indices.R
@@ -3,4 +3,9 @@ testthat::test_that("Correct function output", {
   testthat::expect_type(pr_get_indices("NRS", "Z"), "list")
   testthat::expect_type(pr_get_indices("CPR", "P"), "list")
   testthat::expect_type(pr_get_indices("CPR", "Z"), "list")
+  testthat::expect_type(data.frame(Month = rep(1:12,10),
+                                   StationCode = 'NSI',
+                                   Values = runif(120, min=0, max=10)) %>%
+                          pr_make_climatology("Month"), "list")
+
 })
