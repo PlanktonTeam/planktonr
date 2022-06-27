ae01f526eeffce7a0f0ed57f56b33766e11ba565
Jason Everett <jaseeverett@gmail.com>
2022-06-28 07:52:12 +1000

    Add more tests

--- tests/testthat/test-utils_boo_data.R
+++ tests/testthat/test-utils_boo_data.R
@@ -1,7 +1,13 @@
 testthat::test_that("Correct function output", {
+  testthat::expect_type(planktonr::pr_get_fg("NRS", "P"), "list")
   testthat::expect_type(planktonr::pr_get_fg("NRS", "Z"), "list")
+  testthat::expect_type(planktonr::pr_get_fg("CPR", "P"), "list")
+  testthat::expect_type(planktonr::pr_get_fg("CPR", "Z"), "list")
   testthat::expect_type(pr_get_fMap_data("P"), "list")
+  testthat::expect_type(pr_get_fMap_data("Z"), "list")
+  testthat::expect_type(pr_get_daynight("P"), "list")
   testthat::expect_type(pr_get_daynight("Z"), "list")
+  testthat::expect_type(pr_get_sti("P"), "list")
   testthat::expect_type(pr_get_sti("Z"), "list")
   testthat::expect_type(pr_get_SppCount("Zooplankton"), "list")
   testthat::expect_type(pr_get_facts(), "character")
