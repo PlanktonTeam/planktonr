c55f13abf09051efc06711368721f4cfdff7b9b0
Jason Everett <jaseeverett@gmail.com>
2022-11-17 18:12:33 +1100

    Update tests

--- tests/testthat/test-utils_larvalfish.R
+++ tests/testthat/test-utils_larvalfish.R
@@ -1,3 +1,4 @@
 testthat::test_that("Correct function output", {
   testthat::expect_type(pr_get_LFData(), "list")
+  testthat::expect_type(pr_get_LFData() %>% pr_plot_LarvalFishDist(SpeciesName = "Acanthuridae_37437900", interactive = TRUE), "leaflet")
 })
