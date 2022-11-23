b7a14aa700d5075c1b3039b7fc58aab70689bd35
Jason Everett <jaseeverett@gmail.com>
2022-11-23 13:24:06 +1100

    Update data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,8 +8,6 @@ library(rmapshaper)
 
 devtools::load_all() # Not sure if this is appropriate but I load the package to use the functions to load data
 
-sppSummary <- pr_get_SppCount() # Get the summary info
-
 mbr <- sf::st_read(file.path("data-raw","marine_regions")) %>%  # Load marine regions as sf
   sf::st_transform(crs = "+proj=longlat +datum=WGS84") %>%
   dplyr::select(-c(SHAPE_AREA, SHAPE_LEN)) %>%
@@ -41,7 +39,6 @@ mbr <- tibble(x = c(85, 85:155, 155, 85), y = c(-61, rep(-45, 71), -61, -61)) %>
   dplyr::mutate(REGION = "Southern Ocean Region") %>%
   dplyr::bind_rows(mbr, .)
 
-
 clr <- tibble::tribble( # Set1 on colorbrewer2.org
   ~REGION, ~Colour,
   "North", "#e41a1c",
@@ -53,31 +50,31 @@ clr <- tibble::tribble( # Set1 on colorbrewer2.org
   "Southern Ocean Region", "#a65628")
 
 
-mbr <- dplyr::left_join(mbr, clr, by = "REGION")
+mbr <- dplyr::left_join(mbr, clr, by = "REGION") %>%
+  dplyr::arrange("REGION")
 
-
-
-# imcra_pb <- sf::st_read(file.path("data-raw","imcra_4_pb")) %>%  # Load imcra provincial bioregions as sf
-#   sf::st_transform(crs = "+proj=longlat +datum=WGS84") %>%
-#   dplyr::select(-c(AREA_KM2)) %>%
-#   sf::st_make_valid()
-
-# imcra_meso <- sf::st_read(file.path("data-raw","imcra_4_meso")) %>%  # Load imcra mesoscale bioregions as sf
-#   sf::st_transform(crs = "+proj=longlat +datum=WGS84") %>%
-#   dplyr::select(-c(AREA_KM2, MESO_NUM, MESO_ABBR)) %>%
-#   sf::st_make_valid()
 
 MapOz <- rnaturalearth::ne_countries(scale = "small", country = "Australia",
-                                       returnclass = "sf")
+                                     returnclass = "sf")
 
 meta_sf <- pr_get_NRSTrips("Z") %>%
-  dplyr::select(StationName, StationCode, Longitude, Latitude) %>%
+  dplyr::select("StationName", "StationCode", "Longitude", "Latitude") %>%
   dplyr::distinct() %>%
-  dplyr::rename(Code = StationCode, Station = StationName) %>%
+  dplyr::rename(Code = "StationCode",
+                Station = "StationName") %>%
   dplyr::filter(Station != 'Port Hacking 4') %>%
   sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326)
 
-usethis::use_data(mbr, MapOz, meta_sf, sppSummary, overwrite = TRUE, internal = TRUE, compress = "bzip2")
 
-tools::checkRdaFiles("R") # Check what compression to use above
+colNRS <- data.frame(Code = meta_sf$Code,
+                     Colr = RColorBrewer::brewer.pal(9, "Set1")) %>%
+  tibble::deframe()
+
+colCPR <- mbr %>%
+  sf::st_drop_geometry() %>%
+  tibble::deframe()
+
+usethis::use_data(mbr, MapOz, meta_sf, colNRS, colCPR, overwrite = TRUE, internal = TRUE, compress = "bzip2")
+
+# tools::checkRdaFiles("R") # Check what compression to use above
 # OK - bzip2
