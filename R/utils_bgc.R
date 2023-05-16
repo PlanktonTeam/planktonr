114664aef827884f0efffa6194dbd2b9267b6e01
clairedavies <21113590+clairedavies@users.noreply.github.com>
2023-05-16 11:17:55 +1000

    Update utils_bgc.R

--- R/utils_bgc.R
+++ R/utils_bgc.R
@@ -295,7 +295,7 @@ pr_get_CSChem <- function(){
     dat <- readr::read_csv("https://raw.githubusercontent.com/AusMicrobiome/microbial_ocean_atlas/main/data/oceanViz_AM_data.csv") %>%
       pr_rename() %>%
       dplyr::filter(is.na(.data$TripCode), .data$StationName %in% CoastalStations) %>%
-      dplyr::select("StationName", "SampleDateUTC", "Latitude", "Longitude", tidyselect::any_of(var_names)) %>%
+      dplyr::select("StationName", "SampleDateUTC", "Latitude", "Longitude", SampleDepth_m = "depth_m", tidyselect::any_of(var_names)) %>%
       dplyr::mutate(dplyr::across(tidyselect::all_of(var_names), as.numeric),
                     tz = lutz::tz_lookup_coords(.data$Latitude, .data$Longitude, method = "fast", warn = FALSE))
 
